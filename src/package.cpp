018b5288e69fff064e79361f4d1f56d7e9a7f5fb
Szymon120 <szymonpacyga21@gmail.com>
2023-12-03 16:45:13 +0100

    metody klasy package

--- src/package.cpp
+++ src/package.cpp
@@ -1,3 +1,42 @@
 //
 // Created by adams on 28.11.2023.
 //
+#include "package.hpp"
+
+std::set<ElementID> Package::assigned_IDs = {0};
+std::set<ElementID> Package::freed_IDs = {};
+
+Package::Package() {
+    if (freed_IDs.empty()) {
+        auto it = std::prev(std::end(assigned_IDs));
+        ElementID id = *it + 1;
+        assigned_IDs.insert(id);
+        id_ = id;
+    }
+    else {
+        ElementID id = *freed_IDs.begin();
+        freed_IDs.erase(id);
+        assigned_IDs.insert(id);
+        id_ = id;
+    }
+}
+
+Package::Package(ElementID id) : id_(id) {}
+
+Package::Package(Package && other) {
+    id_ = other.id_;
+    other.id_ = -1;
+}
+
+Package &Package::operator=(Package &&other) {
+    id_ = other.id_;
+    return *this;
+}
+
+Package::~Package() {
+    ElementID id = id_;
+    if (id != -1) {
+        assigned_IDs.erase(id);
+        freed_IDs.insert(id);
+    }
+}
