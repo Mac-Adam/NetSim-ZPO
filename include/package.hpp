9f028112cba440374c17e8c6d4df7506659b9653
Szymon120 <szymonpacyga21@gmail.com>
2023-12-03 16:44:40 +0100

    klasa package

--- include/package.hpp
+++ include/package.hpp
@@ -5,4 +5,26 @@
 #ifndef NETSIM_ZPO_PACKAGE_HPP
 #define NETSIM_ZPO_PACKAGE_HPP
 
+#include <set>
+#include "types.hpp"
+
+class Package {
+
+public:
+
+    Package();
+    Package(ElementID);
+    Package(Package&&);
+    Package& operator=(Package&& other);
+    ElementID get_id() const {return id_;}
+    ~Package();
+
+private:
+
+    ElementID id_;
+
+    static std::set<ElementID> assigned_IDs;
+    static std::set<ElementID> freed_IDs;
+};
+
 #endif //NETSIM_ZPO_PACKAGE_HPP
