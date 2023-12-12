0f4d7fd2df438894f8be299bb838a95636bda938
Szymon120 <129947645+Szymon120@users.noreply.github.com>
2023-12-12 18:05:34 +0100

    Add files via upload

--- factory.hpp
+++ factory.hpp
@@ -3,10 +3,29 @@
 
 #include "nodes.hpp"
 
-class Factory : public Worker : public Storehouse :  public Ramp {
+class Factory : public Worker, public Storehouse,  public Ramp {
 public:
     void add_ramp(&&Ramp r);
     void remove_ramp(ElementID id);
     NodeCollection<Ramp>::iterator find_ramp_by_id(ElementID id);
-};
+    NodeCollection<Ramp>::const_iterator ramp_cbegin();
+    NodeCollection<Ramp>::const_iterator ramp_cend();
+
+    void add_worker(&&Worker w);
+    void remove_worker(ElementID id);
+    NodeCollection<Ramp>::iterator find_worker_by_id(ElementID id);
+    NodeCollection<Ramp>::const_iterator worker_cbegin();
+    NodeCollection<Ramp>::const_iterator worker_cend();
+
+    void add_storehouse(&&Storehouse s);
+    void remove_storehouse(ElementID id);
+    NodeCollection<Ramp>::iterator find_storehouse_by_id(ElementID id);
+    NodeCollection<Ramp>::const_iterator storehouse_cbegin();
+    NodeCollection<Ramp>::const_iterator storehouse_cend();
+
+    bool is_consistent();
+    void do_deliveries(Time t);
+    void do_package_passing();
+    void do_work(Time t);
+    };
 #endif //NETSIM_ZPO_FACTORY_HPP
