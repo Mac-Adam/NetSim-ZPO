e5616c5dfc3c20d9c6f47f7d4f2dedab89467746
Adam Maciuga <80308649+Mac-Adam@users.noreply.github.com>
2023-12-13 11:57:48 +0100

    bugfixes (#12)

--- src/nodes.cpp
+++ src/nodes.cpp
@@ -1,76 +1,80 @@
 
 #include "nodes.hpp"
 
-void ReceiverPreferences::add_receiver(IPackageReceiver *r){
-    double receivers_ = preferences_.size();
+void ReceiverPreferences::add_receiver(IPackageReceiver* r) {
+    std::size_t receivers_ = preferences_.size();
 
-    if(receivers_ == 0)
+    if (receivers_ == 0)
         preferences_[r] = 1.0;
-    if(receivers_ > 0){
-        for(auto &r : preferences_)
-            r.second = 1/(receivers_ + 1);       
+    if (receivers_ > 0) {
+        for (auto& i: preferences_)
+            i.second = 1.0 / ((double) receivers_ + 1.0);
+        preferences_[r] = 1.0 / ((double) receivers_ + 1.0);
     }
 };
 
-void ReceiverPreferences::remove_receiver(IPackageReceiver *r){
-    double receivers_ = preferences_.size();
-    if(receivers_ > 1){
-        for(auto &re : preferences_){
-            if(re.first != r){
-                re.second = 1/(receivers_ - 1);
+void ReceiverPreferences::remove_receiver(IPackageReceiver* r) {
+    std::size_t receivers_ = preferences_.size();
+    if (receivers_ > 1) {
+        for (auto& re: preferences_) {
+            if (re.first != r) {
+                re.second = 1.0 / ((double) receivers_ - 1.0);
             }
         }
     }
     preferences_.erase(r);
 }
 
-IPackageReceiver *ReceiverPreferences::choose_receiver(){
+IPackageReceiver* ReceiverPreferences::choose_receiver() {
     double probal = prob_gen_();
     double distribution = 0;
-    for(auto &receiver : preferences_){
+    for (auto& receiver: preferences_) {
         distribution += receiver.second;
-        if(probal <= distribution){
+        if (probal <= distribution) {
             return receiver.first;
         }
     }
+    return nullptr;
 }
 
-void PackageSender::send_package(){
-    IPackageReceiver *receiver;
+void PackageSender::send_package() {
+    IPackageReceiver* receiver;
 
-    if(buffer_){
+    if (buffer_) {
         receiver = receiver_preferences_.choose_receiver();
-        receiver -> receive_package(std::move(*buffer_));
+        receiver->receive_package(std::move(*buffer_));
         buffer_.reset();
     }
-}   
+}
 
 
 void Ramp::deliver_goods(Time t) {
-    if (t % di_ == 0) {
+    if (t >= last_delivery + di_) {
         push_package(Package());
+        last_delivery = t;
     }
 }
 
-void Worker::recieve_package(Package&& package) {
+void Worker::receive_package(Package&& package) {
     q_->push(std::move(package));
 }
 
 
 void Worker::do_work(Time t) {
+
     if (!buffer_) {
         if (!q_->empty()) {
             buffer_.emplace(q_->pop());
             startTime_ = t;
         }
-    } else if (t >= startTime_ + pd_) {
+    } else if (t + 1 >= startTime_ + pd_) {
         push_package(std::move(buffer_.value()));
         buffer_.reset();
     }
 }
 
-void Storehouse::receive_package(Package&& p){
-    d_.push(this);
+void Storehouse::receive_package(Package&& p) {
+    d_->push(std::move(p));
 }
 
 
