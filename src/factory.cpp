0444e2b7cc4e042d4839676c86573d7b5a61d37d
Szymon120 <szymonpacyga21@gmail.com>
2023-12-21 03:18:24 +0100

    Funkcja is_consistent, has_reachable_storehouse i typ NodeColor.

--- src/factory.cpp
+++ src/factory.cpp
@@ -2,6 +2,7 @@
 // Created by student149 on 12.12.2023.
 //
 
+#include <stdexcept>
 #include "factory.hpp"
 
 void Factory::do_deliveries(Time t) {
@@ -26,3 +27,70 @@ void Factory::do_package_passing() {
     }
 }
 
+enum class NodeColor {
+    UNVISITED, VISITED, VERIFIED
+};
+
+bool has_reachable_storehouse(const PackageSender* sender, std::map<const PackageSender*, NodeColor>& node_colors) {
+    if (node_colors[sender] == NodeColor::VERIFIED) {
+        return true;
+    }
+
+    node_colors[sender] = NodeColor::VISITED;
+
+    std::map<IPackageReceiver*, double> preferences = sender->receiver_preferences_.get_preferences();
+
+    if (preferences.empty()) {
+        throw std::logic_error("");
+    }
+
+    bool senderHasOtherReceiver = false; // does sender have a receiver other than himself?
+
+    for (auto& pair: preferences) {
+        ReceiverType pairType = pair.first->get_receiver_type();
+        PackageSender* sendrecv_ptr = nullptr;
+
+        if (pairType == ReceiverType::STOREHOUSE) {
+            senderHasOtherReceiver = true;
+        } else {
+            IPackageReceiver* receiver_ptr = pair.first;
+            auto worker_ptr = dynamic_cast<Worker*>(receiver_ptr);
+            sendrecv_ptr = dynamic_cast<PackageSender*>(worker_ptr);
+        }
+
+        if (sender == sendrecv_ptr) { continue; }
+        senderHasOtherReceiver = true;
+
+        if (node_colors[sendrecv_ptr] == NodeColor::VISITED) {
+            has_reachable_storehouse(sendrecv_ptr, node_colors);
+        }
+    }
+    node_colors[sender] = NodeColor::VERIFIED;
+
+    if (senderHasOtherReceiver) {
+        return true;
+    } else {
+        throw std::logic_error("");
+    }
+}
+
+bool Factory::is_consistent() {
+
+    std::map<const PackageSender*, NodeColor> node_colors;
+
+    for (auto& worker: workers) {
+        node_colors[dynamic_cast<PackageSender*>(&worker)] = NodeColor::UNVISITED;
+    }
+    for (auto& ramp: ramps) {
+        node_colors[dynamic_cast<PackageSender*>(&ramp)] = NodeColor::UNVISITED;
+    }
+
+    try {
+        for (auto& ramp: ramps) {
+            has_reachable_storehouse(dynamic_cast<PackageSender*>(&ramp), node_colors);
+        }
+    } catch (const std::logic_error&) {
+        return false;
+    }
+    return true;
+}
