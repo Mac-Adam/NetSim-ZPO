b4976b6db44a93d70fcfceadd5a216602a3d1fbd
Adam Maciuga <80308649+Mac-Adam@users.noreply.github.com>
2024-01-01 15:32:20 +0100

    simulation_fix (#25)

--- src/simulation.cpp
+++ src/simulation.cpp
@@ -2,29 +2,24 @@
 #include "simulation.hpp"
 
 bool IntervalReportNotifier::should_generate_report(Time t) {
-    float n = 1 + ((float)t - 1)/(float)to_;
-    if (std::floor(n) == n) {
-        return true;
-    }
-    return false;
+    return (t - 1) % to_ == 0;
 }
 
 bool SpecificTurnsReportNotifier::should_generate_report(Time t) {
-    if (turns_.find(t) != turns_.end()) {
-        return true;
-    }
-    return false;
+    return turns_.find(t) != turns_.end();
 }
 
-void simulate(Factory& f, TimeOffset d, std::function<void (Factory&, Time)> rf) {
+void simulate(Factory& f, TimeOffset d, std::function<void(Factory&, Time)> rf) {
     if (!f.is_consistent()) {
         throw std::logic_error("");
     }
 
     for (Time simulationTime = 1; simulationTime <= d; simulationTime++) {
+
         f.do_deliveries(simulationTime);
-        f.do_work(simulationTime);
         f.do_package_passing();
+        f.do_work(simulationTime);
+
         rf(f, simulationTime);
     }
 }
